c2bf6930b28bf778f79b2cb61a2db7ae2be3c20a
Kucherenko Mykyta <Kucherenko Mykyta@users.noreply.github.com>
2019-05-30 17:53:41 +0300

    Rewrote to use messageSource.

--- src/main/java/edu/practice/authorizationServer/model/service/impl/UserDetailsServiceImpl.java
+++ src/main/java/edu/practice/authorizationServer/model/service/impl/UserDetailsServiceImpl.java
@@ -4,6 +4,8 @@ import edu.practice.authorizationServer.model.entity.ApplicationUser;
 import edu.practice.authorizationServer.model.entity.ApplicationUserDetailsImpl;
 import edu.practice.authorizationServer.model.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -18,9 +20,12 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
     private UserRepository userRepository;
 
+    private MessageSource messageSource;
+
     @Autowired
-    public UserDetailsServiceImpl(UserRepository userRepository) {
+    public UserDetailsServiceImpl(UserRepository userRepository, MessageSource messageSource) {
         this.userRepository = userRepository;
+        this.messageSource = messageSource;
     }
 
     @Override
@@ -31,8 +36,13 @@ public class UserDetailsServiceImpl implements UserDetailsService {
                 ExampleMatcher.matching().withIgnoreCase("username"));
         Optional<ApplicationUser> persistedUserOptional = userRepository.findOne(persistedApplicationUserExample);
         if (!persistedUserOptional.isPresent())
-            // TODO: rewrite in case of introducing messageSource
-            throw new UsernameNotFoundException("User " + username + " not found.");
+            throw new UsernameNotFoundException(
+                messageSource.getMessage(
+                    "userDetailsServiceImpl.usernameNotFoundException.message",
+                    new Object[] {
+                        username
+                    },
+                    LocaleContextHolder.getLocale()));
 
         ApplicationUser persistedUser = persistedUserOptional.get();
         return ApplicationUserDetailsImpl.builder()
