63b730b24b3f54ece9681e5549dad6f27a8694ce
Kucherenko Mykyta <Kucherenko Mykyta@users.noreply.github.com>
2019-05-30 09:40:09 +0300

    Rewrote UserDetails implementation to use @Builder

--- src/main/java/edu/practice/authorizationServer/model/service/impl/UserDetailsServiceImpl.java
+++ src/main/java/edu/practice/authorizationServer/model/service/impl/UserDetailsServiceImpl.java
@@ -1,18 +1,16 @@
 package edu.practice.authorizationServer.model.service.impl;
 
 import edu.practice.authorizationServer.model.entity.ApplicationUser;
+import edu.practice.authorizationServer.model.entity.ApplicationUserDetailsImpl;
 import edu.practice.authorizationServer.model.repository.UserRepository;
-import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.Collection;
 import java.util.Optional;
 
 @Service
@@ -37,55 +35,11 @@ public class UserDetailsServiceImpl implements UserDetailsService {
             throw new UsernameNotFoundException("User " + username + " not found.");
 
         ApplicationUser persistedUser = persistedUserOptional.get();
-        return new UserDetailsImpl(
-                persistedUser.getRole().getAuthorities(),
-                persistedUser.getPassword(),
-                persistedUser.getUsername(),
-                persistedUser.isEnabled());
-    }
-
-    @AllArgsConstructor
-    private class UserDetailsImpl implements UserDetails {
-
-        private static final long serialVersionUID = 1L;
-        private Collection<? extends GrantedAuthority> grantedAuthorities;
-        private String password;
-        private String username;
-        private boolean enabled;
-
-        @Override
-        public Collection<? extends GrantedAuthority> getAuthorities() {
-            return this.grantedAuthorities;
-        }
-
-        @Override
-        public String getPassword() {
-            return this.password;
-        }
-
-        @Override
-        public String getUsername() {
-            return this.username;
-        }
-
-        @Override
-        public boolean isAccountNonExpired() {
-            return this.enabled;
-        }
-
-        @Override
-        public boolean isAccountNonLocked() {
-            return this.enabled;
-        }
-
-        @Override
-        public boolean isCredentialsNonExpired() {
-            return this.enabled;
-        }
-
-        @Override
-        public boolean isEnabled() {
-            return this.enabled;
-        }
+        return ApplicationUserDetailsImpl.builder()
+                .authorities(persistedUser.getRole().getAuthorities())
+                .password(persistedUser.getPassword())
+                .username(persistedUser.getUsername())
+                .enabled(persistedUser.isEnabled())
+                .build();
     }
 }
